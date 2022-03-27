cb8b9c1b25d440683797404aef15cbd3efe62638
George-Okumu <gokumu12@gmail.com>
2022-03-27 08:26:52 +0300

    assignment compelete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    let submission = document.getElementById("create-task-form");
+    submission.addEventListener("submit", function(e) {
+        e.preventDefault();
+        // alert("Well job");
+        let usersInput = document.getElementById("new-task-description").value;
+        console.log(usersInput);
+
+
+        let li = document.createElement("li");
+        li.append(usersInput);
+        document.getElementById("tasks").appendChild(li);
+
+    });
 });
